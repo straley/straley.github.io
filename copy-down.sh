a29e0568025bb163af2f03fc01251ad349495c1a
Scott Straley <scottstraley@gmail.com>
2018-01-31 01:24:54 -0600

    copy-down

--- copy-down.sh
+++ copy-down.sh
@@ -5,6 +5,7 @@ cp build/favicon.ico .
 cp build/index.html .
 cp build/manifest.json .
 cp build/service-worker.js .
+cp build/scottstraley.pdf .
 rm static/css/*
 cp build/static/css/* static/css/
 rm static/js/*
