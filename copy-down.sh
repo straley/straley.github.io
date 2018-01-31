d600621012b9d0cce35ca1e7ecb6d25b91aab742
Scott Straley <scottstraley@gmail.com>
2018-01-30 20:42:03 -0600

    copy-down now rules

--- copy-down.sh
+++ copy-down.sh
@@ -7,4 +7,8 @@ cp build/manifest.json .
 cp build/service-worker.js .
 cp build/static/css/* static/css/
 cp build/static/js/* static/js/
-cp build/static/meda/* static/media/
+cp build/static/media/* static/media/
+git add asset-manifest.json index.html service-worker.js
+git add static/*
+git commit -m "copy-down"
+git push
