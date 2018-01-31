21dcf0da1066a27f726a03c843cb9e0333cf3a35
Scott Straley <scottstraley@gmail.com>
2018-01-31 00:43:35 -0600

    copy-down

--- copy-down.sh
+++ copy-down.sh
@@ -5,8 +5,11 @@ cp build/favicon.ico .
 cp build/index.html .
 cp build/manifest.json .
 cp build/service-worker.js .
+rm static/css/*
 cp build/static/css/* static/css/
+rm static/js/*
 cp build/static/js/* static/js/
+rm static/media/*
 cp build/static/media/* static/media/
 git add asset-manifest.json index.html service-worker.js
 git add static/*
